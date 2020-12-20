3c7330dac55f75279b0d4ab9ecd0370fb423fde0
Taiwo Famuyiwa <tincorpai@gmail.com>
2020-12-19 21:34:37 -0600

    The data manipulation

--- DataManipulationVisualization.R
+++ DataManipulationVisualization.R
@@ -7,6 +7,7 @@ library(tidyverse)
 
 
 
+
 ###################
 #Load the DataSet #
 ###################
@@ -31,22 +32,51 @@ data_iris <- iris
 #and it is sometimes called a modified leave.
 #For the Petal is each segments of the corolla of a
 #flower and they are modified leaves and they are 
-#typically colored.
+#typically colored. The length and width of Sepal and
+#Petal were measured for three species of the 
+#leaf.The number of rows and columns 150 and 5 
+#respectively.
+summary(data_iris)
+ncol(data_iris)
+nrow(data_iris)
 
-
-
-
-summary(iris)
 
 #Check For Missing Data#
 #use is.na() function to check for a missing data
 #is.na() will issue a boolean False statement is a
-#missing value exist otherwise issue a True statement.
-is.na(iris)
-sum(is.na(iris))
-class(iris)
+#missing value exist otherwise issue a True 
+#statement.
+is.na(data_iris)
+sum(is.na(data_irisiris))
+class(data_iris)
 
 ###################
 #Data Manipulation#
 ###################
+#This code will sum Petal width and Petal length for
+#each class of species.
+#From this code, it is noticeable from the output 
+#that virginica has the highest number of Petal length
+# with total number of 278 centimeter across all 
+#observations. This is follow by versicolor 
+#with total sum of Petal length and Petal width to be
+#213 and 66.3 centimeters respectively.
+iris_Petal <- data_iris %>% 
+  select(Petal.Length, Petal.Width, Species) %>%
+  group_by(Species)%>%
+  summarize(Petlength_sum = sum(Petal.Length),
+            petwidth_sum = sum(Petal.Width), .groups = 'drop')
 
+
+#Just like what at the previous result, it also
+#noticeable here that specie virginica has the sum
+#of sepal length and width respectively follow by versicolor
+#specie.
+iris_Sepal <- data_iris %>% 
+  select(Sepal.Length,Sepal.Width, Species) %>%
+  group_by(Species)%>%
+  summarize(Sepallength_sum = sum(Sepal.Length),
+            Sepalwidth_sum = sum(Sepal.Width), .groups = 'drop')
+  
+  
+#The following barcha
